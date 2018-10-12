52f96e8e7fac2d4e569c72fd649072563b0f5061
Jon <jonathan.furmedge@staffs.ac.uk>
2018-10-12 12:43:12 +0100

    Initial commit

---
+++ Source/EventDispatchers/EventDispatchersGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EventDispatchersGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEventDispatchersGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEventDispatchersGameMode();
+};
+
+
+
